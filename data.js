6b12ca3c323e9bae910317c2ac4fbb621bc9a73c
Basilio Cáceres <basilio@bloom-ux.com>
2023-01-09 13:51:46 -0300

    ajustes en datos y estilos tipográficos

--- data.js
+++ data.js
@@ -1,188 +1,5 @@
 const groups = [
 	{
-		name: 'Cereales, Papas y Leguminosas frescas',
-		portions: null,
-		subgroups: [
-			{
-				name: 'Cereales, Papas y Leguminosas frescas',
-				portion: null,
-				calories: 140,
-				cho: '30g',
-				protein: '3g',
-				fat: '1g',
-				food_types: [
-					{
-						name: 'Arroz, Arroz integral, Fideos, Fideos integrales',
-						values: [
-							{
-								unit: 'taza',
-								value: '3/4'
-							}
-						]
-					},
-					{
-						name: 'Quínoa (pseudo cereal)',
-						values: [
-							{
-								unit: 'taza',
-								value: '1/4'
-							},
-							{
-								unit: 'cdas',
-								value: '4'
-							},
-						]
-					},
-					{
-						name: 'Arvejas',
-						values: [
-							{
-								unit: 'tazas',
-								value: '1 1/2'
-							},
-						]
-					},
-					{
-						name: 'Choclo',
-						values: [
-							{
-								unit: 'taza',
-								value: '3/4'
-							}
-						]
-					},
-					{
-						name: 'Porotos granados',
-						values: [
-							{
-								unit: 'taza',
-								value: '3/4'
-							}
-						]
-					},
-					{
-						name: 'Papa',
-						values: [
-							{
-								unit: 'unidad regular',
-								value: '1'
-							}
-						]
-					},
-					{
-						name: 'Haba',
-						values: [
-							{
-								unit: 'taza',
-								value: '1'
-							}
-						]
-					},
-					{
-						name: 'Galletas de agua o soda',
-						values: [
-							{
-								unit: 'unidades',
-								value: '8'
-							}
-						]
-					},
-					{
-						name: 'Pan marraqueta',
-						values: [
-							{
-								unit: 'unidad',
-								value: '1/2'
-							},
-							{
-								unit: 'grs',
-								value: '50'
-							},
-						]
-					},
-					{
-						name: 'Pan Hallulla',
-						values: [
-							{
-								unit: 'unidad',
-								value: '1/2'
-							},
-							{
-								unit: 'g',
-								value: '50'
-							},
-						]
-					},
-					{
-						name: 'Pan Molde',
-						values: [
-							{
-								unit: 'rebanadas',
-								value: '3'
-							},
-							{
-								unit: 'g',
-								value: '60'
-							},
-						]
-					},
-					{
-						name: 'Pan integral Hallulla',
-						values: [
-							{
-								unit: 'unidad chica',
-								value: '1/2'
-							},
-							{
-								unit: 'g',
-								value: '50'
-							},
-						]
-					},
-					{
-						name: 'Pan integral molde',
-						values: [
-							{
-								unit: 'rebanada',
-								value: '1 1/4'
-							},
-							{
-								unit: 'g',
-								value: '50'
-							},
-						]
-					},
-					{
-						name: 'Pan Centeno Molde',
-						values: [
-							{
-								unit: 'rebanada',
-								value: '1 1/4'
-							},
-							{
-								unit: 'g',
-								value: '50'
-							},
-						]
-					},
-					{
-						name: 'Pan Centeno Hallulla',
-						values: [
-							{
-								unit: 'unidad chica',
-								value: '1'
-							},
-							{
-								unit: 'g',
-								value: '50'
-							},
-						]
-					}
-				]
-			}
-		]
-	},
-	{
 		name: 'Verduras',
 		portions: null,
 		subgroups: [
@@ -319,6 +136,171 @@ const groups = [
 								unit: 'unidades',
 								value: '5'
 							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		name: 'Cereales, Papas y Leguminosas frescas',
+		portions: null,
+		subgroups: [
+			{
+				name: 'Cereales, Papas y Leguminosas frescas',
+				portion: null,
+				calories: 140,
+				cho: '30g',
+				protein: '3g',
+				fat: '1g',
+				food_types: [
+					{
+						name: 'Arroz, arroz integral, fideos, fideos integrales, choclo, porotos granados',
+						values: [
+							{
+								unit: 'taza',
+								value: '3/4'
+							}
+						]
+					},
+					{
+						name: 'Quínoa (pseudo cereal)',
+						values: [
+							{
+								unit: 'taza',
+								value: '1/4'
+							},
+							{
+								unit: 'cdas',
+								value: '4'
+							},
+						]
+					},
+					{
+						name: 'Arvejas',
+						values: [
+							{
+								unit: 'tazas',
+								value: '1 1/2'
+							},
+						]
+					},
+					{
+						name: 'Papa',
+						values: [
+							{
+								unit: 'unidad regular',
+								value: '1'
+							}
+						]
+					},
+					{
+						name: 'Haba',
+						values: [
+							{
+								unit: 'taza',
+								value: '1'
+							}
+						]
+					},
+					{
+						name: 'Galletas de agua o soda',
+						values: [
+							{
+								unit: 'unidades',
+								value: '8'
+							}
+						]
+					},
+					{
+						name: 'Pan marraqueta',
+						values: [
+							{
+								unit: 'unidad',
+								value: '1/2'
+							},
+							{
+								unit: 'grs',
+								value: '50'
+							},
+						]
+					},
+					{
+						name: 'Pan Hallulla',
+						values: [
+							{
+								unit: 'unidad',
+								value: '1/2'
+							},
+							{
+								unit: 'g',
+								value: '50'
+							},
+						]
+					},
+					{
+						name: 'Pan Molde',
+						values: [
+							{
+								unit: 'rebanadas',
+								value: '3'
+							},
+							{
+								unit: 'g',
+								value: '60'
+							},
+						]
+					},
+					{
+						name: 'Pan integral Hallulla',
+						values: [
+							{
+								unit: 'unidad chica',
+								value: '1/2'
+							},
+							{
+								unit: 'g',
+								value: '50'
+							},
+						]
+					},
+					{
+						name: 'Pan integral molde',
+						values: [
+							{
+								unit: 'rebanada',
+								value: '1 1/4'
+							},
+							{
+								unit: 'g',
+								value: '50'
+							},
+						]
+					},
+					{
+						name: 'Pan Centeno Molde',
+						values: [
+							{
+								unit: 'rebanada',
+								value: '1 1/4'
+							},
+							{
+								unit: 'g',
+								value: '50'
+							},
+						]
+					},
+					{
+						name: 'Pan Centeno Hallulla',
+						values: [
+							{
+								unit: 'unidad chica',
+								value: '1'
+							},
+							{
+								unit: 'g',
+								value: '50'
+							},
 						]
 					}
 				]
@@ -638,6 +620,165 @@ const groups = [
 		]
 	},
 	{
+		name: 'Frutas',
+		portions: null,
+		subgroups: [
+			{
+				name: 'Frutas',
+				portion: null,
+				calories: 65,
+				cho: '15g',
+				protein: '1g',
+				fat: '0g',
+				food_types: [
+					{
+						name: 'Manzana, caqui, durazno, membrillo, naranja, pera',
+						values: [
+							{
+								unit: 'unidad',
+								value: '1'
+							},
+							{
+								unit: 'g',
+								value: '100'
+							}
+						]
+					},
+					{
+						name: 'Ciruelas, damasco, limón, papaya',
+						values: [
+							{
+								unit: 'unidades',
+								value: '3'
+							}
+						]
+					},
+					{
+						name: 'Higos, kiwi, tuna',
+						values: [
+							{
+								unit: 'unidades',
+								value: '2'
+							},
+						]
+					},
+					{
+						name: 'Pepino dulce',
+						values: [
+							{
+								unit: 'unidad grande',
+								value: '1'
+							}
+						]
+					},
+					{
+						name: 'Plátano',
+						values: [
+							{
+								unit: 'unidad',
+								value: '1/2'
+							},
+							{
+								unit: 'g',
+								value: '60'
+							}
+						]
+					},
+					{
+						name: 'Chirimoya',
+						values: [
+							{
+								unit: 'unidad',
+								value: '1/4'
+							}
+						]
+					},
+					{
+						name: 'Cerezas',
+						values: [
+							{
+								unit: 'unidades',
+								value: '15'
+							}
+						]
+					},
+					{
+						name: 'Uvas',
+						values: [
+							{
+								unit: 'unidades',
+								value: '10'
+							}
+						]
+					},
+					{
+						name: 'Frambuesas, frutillas, melón, sandia',
+						values: [
+							{
+								unit: 'taza',
+								value: '1'
+							}
+						]
+					},
+					{
+						name: 'Mora, grosella',
+						values: [
+							{
+								unit: 'taza',
+								value: '1/2'
+							}
+						]
+					},
+					{
+						name: 'Piña',
+						values: [
+							{
+								unit: 'taza',
+								value: '3/4'
+							}
+						]
+					},
+					{
+						name: 'Higos secos, huesillo',
+						values: [
+							{
+								unit: 'unidades',
+								value: '2'
+							}
+						]
+					},
+					{
+						name: 'Pasas',
+						values: [
+							{
+								unit: 'unidades',
+								value: '20'
+							}
+						]
+					},
+					{
+						name: 'Ciruelas',
+						values: [
+							{
+								unit: 'unidades',
+								value: '2 1/2'
+							}
+						]
+					},
+					{
+						name: 'Pera desecada, manzana desecada',
+						values: [
+							{
+								unit: 'rodelas',
+								value: '7'
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
 		name: 'Aceites y grasas',
 		portions: null,
 		subgroups: [
@@ -799,165 +940,6 @@ const groups = [
 		]
 	},
 	{
-		name: 'Frutas',
-		portions: null,
-		subgroups: [
-			{
-				name: 'Frutas',
-				portion: null,
-				calories: 65,
-				cho: '15g',
-				protein: '1g',
-				fat: '0g',
-				food_types: [
-					{
-						name: 'Manzana, caqui, durazno, membrillo, naranja, pera',
-						values: [
-							{
-								unit: 'unidad',
-								value: '1'
-							},
-							{
-								unit: 'g',
-								value: '100'
-							}
-						]
-					},
-					{
-						name: 'Ciruelas, damasco, limón, papaya',
-						values: [
-							{
-								unit: 'unidades',
-								value: '3'
-							}
-						]
-					},
-					{
-						name: 'Higos, kiwi, tuna',
-						values: [
-							{
-								unit: 'unidades',
-								value: '2'
-							},
-						]
-					},
-					{
-						name: 'Pepino dulce',
-						values: [
-							{
-								unit: 'unidad grande',
-								value: '1'
-							}
-						]
-					},
-					{
-						name: 'Plátano',
-						values: [
-							{
-								unit: 'unidad',
-								value: '1/2'
-							},
-							{
-								unit: 'g',
-								value: '60'
-							}
-						]
-					},
-					{
-						name: 'Chirimoya',
-						values: [
-							{
-								unit: 'unidad',
-								value: '1/4'
-							}
-						]
-					},
-					{
-						name: 'Cerezas',
-						values: [
-							{
-								unit: 'unidades',
-								value: '15'
-							}
-						]
-					},
-					{
-						name: 'Uvas',
-						values: [
-							{
-								unit: 'unidades',
-								value: '10'
-							}
-						]
-					},
-					{
-						name: 'Frambuesas, frutillas, melón, sandia',
-						values: [
-							{
-								unit: 'taza',
-								value: '1'
-							}
-						]
-					},
-					{
-						name: 'Mora, grosella',
-						values: [
-							{
-								unit: 'taza',
-								value: '1/2'
-							}
-						]
-					},
-					{
-						name: 'Piña',
-						values: [
-							{
-								unit: 'taza',
-								value: '3/4'
-							}
-						]
-					},
-					{
-						name: 'Higos secos, huesillo',
-						values: [
-							{
-								unit: 'unidades',
-								value: '2'
-							}
-						]
-					},
-					{
-						name: 'Pasas',
-						values: [
-							{
-								unit: 'unidades',
-								value: '20'
-							}
-						]
-					},
-					{
-						name: 'Ciruelas',
-						values: [
-							{
-								unit: 'unidades',
-								value: '2 1/2'
-							}
-						]
-					},
-					{
-						name: 'Pera desecada, manzana desecada',
-						values: [
-							{
-								unit: 'rodelas',
-								value: '7'
-							}
-						]
-					}
-				]
-			}
-		]
-	},
-	{
 		name: 'Carnes, Pescados, Huevos y Leguminosas secas',
 		portions: null,
 		subgroups: [
@@ -1041,7 +1023,7 @@ const groups = [
 				fat: '0g',
 				food_types: [
 					{
-						name: 'Azúcar, miel de abeja, miel de palma, mermeladas, dulce de membrillo, manjar, jalea en polvo, jugo en polvo',
+						name: 'Azúcar, miel de abeja, miel de palma, mermeladas, dulce de membrillo, manjar, jalea y jugo en polvo',
 						values: [
 							{
 								unit: 'cdta',
